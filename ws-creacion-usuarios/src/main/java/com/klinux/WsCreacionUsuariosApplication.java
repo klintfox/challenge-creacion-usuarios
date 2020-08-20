5c213cbe8ee90e934e2cc6fca46736f657cc8d99
klinux <klint_f@hotmail.com>
2020-08-20 14:21:38 -0500

    init project

---
+++ ws-creacion-usuarios/src/main/java/com/klinux/WsCreacionUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.klinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsCreacionUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsCreacionUsuariosApplication.class, args);
+	}
+
+}
